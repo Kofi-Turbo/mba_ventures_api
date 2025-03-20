ce2976bc438c3d48032c4b45f2a611ac38828efb
Daniel G. Amoateng <f005cm3@dartmouth.edu>
2025-03-20 04:19:15 -0400

    Created .env

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAezayMemxxN9SeT00f7eAjmwIJnRfyLBQ",
-  authDomain: "mba-ventures-80390.firebaseapp.com",
-  projectId: "mba-ventures-80390",
-  storageBucket: "mba-ventures-80390.firebasestorage.app",
-  messagingSenderId: "902709668201",
-  appId: "1:902709668201:web:0cd3a271f5c015023df5f8",
-  measurementId: "G-PJXMDGQTFB"
-};
+const firebaseConfig = process.env.fireBaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
